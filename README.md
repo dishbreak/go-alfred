401cf263e955c576fb9dcf45b6e9d622ddd91743
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2024-11-15 11:33:40 -0800

    feat!: version 2 overhaul

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Alfred Workflows in Go
 
 This is a work-in-progress, but it aims to implement the JSON interface that Alfred for macOS expects when it executes commands in its workflows.
+
+The Version 2 rewrite attempts to make the library much more lightweight. It leaves items like configuration and execution control to the caller and focuses on sending appropriate responses back to Alfred using its JSON format.
+
+Version 2 requires Alfred 5 or later in order to take advantage of things like Alfred's built-in caching options.
---
+++ v2/alfred/alfred_test.go
@@ -0,0 +1,48 @@
+package alfred
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewScriptFilterResponse(t *testing.T) {
+
+	sr := NewScriptFilterResponse()
+	assert.Empty(t, sr.Items)
+}
+
+func TestScriptFilterResponseAddItems(t *testing.T) {
+	output := bytes.NewBufferString("")
+
+	sr := NewScriptFilterResponse(ScriptFilterWithOutput(output))
+
+	sr.AddItem(ListItem{
+		Title:     "foo",
+		Subtitle:  "bar",
+		Arg:       "fool",
+		Variables: map[string]string{},
+	})
+
+	sr.SendFeedback()
+	assert.Equal(t, `{"items":[{"title":"foo","subtitle":"bar","arg":"fool","valid":false,"variables":{}}]}
+`, output.String())
+}
+
+func TestScriptOutputWithCache(t *testing.T) {
+	output := bytes.NewBufferString("")
+
+	sr := NewScriptFilterResponse(ScriptFilterWithOutput(output), ScriptFilterWithCache(60, false))
+	sr.AddItem(ListItem{
+		Title:     "foo",
+		Subtitle:  "bar",
+		Arg:       "fool",
+		Variables: map[string]string{},
+	})
+
+	sr.SendFeedback()
+	assert.Equal(t, `{"items":[{"title":"foo","subtitle":"bar","arg":"fool","valid":false,"variables":{}}],"cache":{"seconds":60,"loosereload":false}}
+`, output.String())
+}
