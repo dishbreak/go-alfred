6f3c4eff0ff28f902bf32932dd81ffeb9899e715
Vishal Kotcherlakota <dishbreak@gmail.com>
2024-11-15 11:34:28 -0800

    feat!: version 2 overhaul (#2)
    * feat!: version 2 overhaul
    
    * Merge remote-tracking branch 'origin/main' into version2-upgrade

---
+++ v2/alfred/constants.go
@@ -0,0 +1,12 @@
+package alfred
+
+const (
+	SettingsDir      string = ".alfred_settings"
+	SettingsFilename string = "settings.json"
+	ExecStatus       string = "status"
+	StatusFailed     string = "failed"
+	StatusOk         string = "ok"
+	MessageTitle     string = "message_title"
+	MessageBody      string = "message_body"
+	CacheWorkspace   string = "workspaces"
+)
